ccd50a588b2ec214f067af10783c92440a18c9c7
Nico Proto <nicolasproto@gmail.com>
2020-10-13 11:32:59 +0300

    Added CRUD actions and Front End styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Create Read Update Destroy
+  # VERB path, to: 'controller#action'
+
+  # Read all the restaurants
+  get '/restaurants', to: 'restaurants#index'
+
+  # Create a restaurant
+  get '/restaurants/new', to: 'restaurants#new',  as: :new_restaurant
+  post '/restaurants', to: 'restaurants#create'
+
+  # Read one restaurant
+  get '/restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  # Update a restaurant
+  get '/restaurants/:id/edit', to: 'restaurants#edit', as: :edit_restaurant
+  patch '/restaurants/:id', to: 'restaurants#update'
+
+  # Destroy a restaurant
+  delete '/restaurants/:id', to: 'restaurants#destroy'
+
+  # resources :restaurants
 end
